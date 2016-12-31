840fc6a8b40cab9a784d9ea7daf4aa9cbe347fe8
Yongcai Liu <yoli@microsoft.com>
2016-12-30 16:51:51 -0800

    Question 1

---
+++ projecteuler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projecteuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AnswerQuestion1(1000));
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// https://projecteuler.net/problem=1
+        /// </summary>
+        /// <returns></returns>
+        static int AnswerQuestion1(int upLimit)
+        {
+            int ret = 0;
+            for (int i = 3; i < upLimit; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    ret += i;
+                }
+            }
+            return ret;
+        }
+    }
+}
